6db656d2e394a39da2437290ef4625855d5d180d
Vu Ngoc Nhat <khongthichprov2@gmail.com>
2024-08-16 15:20:40 +0700

    rotation from left-right, top-bottom

--- src/Main.java
+++ src/Main.java
@@ -35,6 +35,7 @@ public class Main {
         JSlider headingSlider = new JSlider(0 , 360, 180);
         pane.add(headingSlider, BorderLayout.SOUTH);
 
+
         // slider to control vertical rotation
         JSlider pitchSlider = new JSlider(SwingConstants.VERTICAL,-90, 90, 0);
         pane.add(pitchSlider, BorderLayout.EAST);
@@ -42,17 +43,38 @@ public class Main {
         //panel to display render results
         JPanel renderPanel = new JPanel(){
             public void paintComponent(Graphics g) {
+
+                double heading = Math.toRadians(headingSlider.getValue());
+                //XZ rotation matrix, rotation left-right
+                Matrix3 headingTransform = new Matrix3(new double[]{
+                        Math.cos(heading), 0, Math.sin(heading),
+                        0,1,0,
+                        -Math.sin(heading), 0 , Math.cos(heading)
+                });
+
+                double pitch = Math.toRadians(pitchSlider.getValue());
+                //YZ rotation matrix, rotation top-bottom
+                Matrix3 pitchTransform = new Matrix3(new double[]{
+                        1,0,0,
+                        0, Math.cos(pitch), Math.sin(pitch),
+                        0, -Math.sin(pitch), Math.cos(pitch)
+                });
+                Matrix3 transform = headingTransform.multiply(pitchTransform);
                 Graphics2D g2 = (Graphics2D) g;
                 g2.setColor(Color.black);
                 g2.fillRect(0,0,getWidth(),getHeight());
                 //rendering magic will happen here
                 g2.translate(getWidth() / 2, getHeight() / 2);
                 g2.setColor(Color.WHITE);
+                //draw triangle
                 for (Triangle t : tris) {
+                    Vertex v1 = transform.transform(t.v1);
+                    Vertex v2 = transform.transform(t.v2);
+                    Vertex v3 = transform.transform(t.v3);
                     Path2D path = new Path2D.Double();
-                    path.moveTo(t.v1.x, t.v1.y);
-                    path.lineTo(t.v2.x, t.v2.y);
-                    path.lineTo(t.v3.x, t.v3.y);
+                    path.moveTo(v1.x, v1.y);
+                    path.lineTo(v2.x, v2.y);
+                    path.lineTo(v3.x, v3.y);
                     path.closePath();
                     g2.draw(path);
                 }
@@ -60,10 +82,11 @@ public class Main {
         };
         pane.add(renderPanel, BorderLayout.CENTER);
 
+        // Add ChangeListeners to sliders
+        headingSlider.addChangeListener(e -> renderPanel.repaint());
+        pitchSlider.addChangeListener(e -> renderPanel.repaint());
+
         frame.setSize(400, 400);
         frame.setVisible(true);
-
-
     }
-
 }
