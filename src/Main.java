7c2351c4b97a54a9e258e3ab509a188e3cb3cbe0
Vu Ngoc Nhat <khongthichprov2@gmail.com>
2024-08-16 11:14:14 +0700

    draw triangle

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,31 @@
+import model.Triangle;
+import model.Vertex;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Path2D;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        List<Triangle> tris = new ArrayList<>();
+        tris.add(new Triangle(new Vertex(100, 100, 100),
+                new Vertex(-100, -100, 100),
+                new Vertex(-100, 100, -100),
+                Color.WHITE));
+        tris.add(new Triangle(new Vertex(100, 100, 100),
+                new Vertex(-100, -100, 100),
+                new Vertex(100, -100, -100),
+                Color.RED));
+        tris.add(new Triangle(new Vertex(-100, 100, -100),
+                new Vertex(100, -100, -100),
+                new Vertex(100, 100, 100),
+                Color.GREEN));
+        tris.add(new Triangle(new Vertex(-100, 100, -100),
+                new Vertex(100, -100, -100),
+                new Vertex(-100, -100, 100),
+                Color.BLUE));
         //
         JFrame frame = new JFrame();
         Container pane = frame.getContentPane();
@@ -23,11 +46,24 @@ public class Main {
                 g2.setColor(Color.black);
                 g2.fillRect(0,0,getWidth(),getHeight());
                 //rendering magic will happen here
+                g2.translate(getWidth() / 2, getHeight() / 2);
+                g2.setColor(Color.WHITE);
+                for (Triangle t : tris) {
+                    Path2D path = new Path2D.Double();
+                    path.moveTo(t.v1.x, t.v1.y);
+                    path.lineTo(t.v2.x, t.v2.y);
+                    path.lineTo(t.v3.x, t.v3.y);
+                    path.closePath();
+                    g2.draw(path);
+                }
             }
         };
         pane.add(renderPanel, BorderLayout.CENTER);
 
         frame.setSize(400, 400);
         frame.setVisible(true);
+
+
     }
+
 }
